19ca5a575409c60451790d488b0f2cb30021921c
Rishav Raj <rajrishav011@gmail.com>
2025-09-20 12:03:25 +0530

    update file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const BASE_URL = "https://o-chat-vfpl.onrender.com";
+// "http://localhost:5001/api"
+const BASE_URL = import.meta.env.MODE === "production" ? "https://o-chat-vfpl.onrender.com/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
